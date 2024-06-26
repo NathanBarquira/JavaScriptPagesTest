38f392c563e1c27cbfb6259bb6e86faceb90addf
Nathan Barquira <nathanbarquira@gmail.com>
2024-06-25 22:46:03 -0700

    making flatbuffers global

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 // importing flatbuffers
 var flatbuffers = require('flatbuffers').flatbuffers;
+window.flatbuffers = flatbuffers;  // making it global cuz laziness
 console.log("FlatBuffers is loaded", flatbuffers);
